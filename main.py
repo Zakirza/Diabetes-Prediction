1fbb87f8e82f231bcba41f8fbeda6bb040041f11
MOHD ZAKIR <mohdzakeer011@gmail.com>
2026-03-05 15:03:16 +0530

    Remove e from print statements in main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src.evaluation import evaluate_models
 from src.fine_tuning import tune_ann_model, tune_cnn_model
 
 def main():
-    print("🚀 Starting Hybrid Ensemble Diabetes Prediction")
+    print(" Starting Hybrid Ensemble Diabetes Prediction")
 
     # Load data
     (X_train, X_test, y_train, y_test), scaler = load_and_preprocess_data("data/Diabetes.csv")
@@ -14,10 +14,10 @@ def main():
     base_models = train_base_models(X_train, y_train)
 
     # Fine-tuning (RUN ONLY ONCE — SAVES BEST MODELS)
-    print("\n🔧 Fine-tuning ANN...")
+    print("\n Fine-tuning ANN...")
     best_ann = tune_ann_model(X_train, y_train)
 
-    print("\n🔧 Fine-tuning CNN...")
+    print("\n Fine-tuning CNN...")
     best_cnn = tune_cnn_model(X_train, y_train)
 
     base_models["ANN_Tuned"] = best_ann
@@ -29,7 +29,7 @@ def main():
     # Evaluate everything
     evaluate_models(base_models, hybrid_model, X_test, y_test)
 
-    print("\n🎉 All steps completed successfully!")
+    print("\n All steps completed successfully!")
 
 if __name__ == "__main__":
     main()
